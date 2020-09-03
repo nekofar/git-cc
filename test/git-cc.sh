383e15bf1b7152bb76cdb5f4c0828abc632007ac
Milad Nekofar <milad@nekofar.com>
2020-09-03 22:59:18 +0430

    test: skip some tests on lower bash versions

--- test/git-cc.sh
+++ test/git-cc.sh
@@ -6,30 +6,40 @@ oneTimeSetUp() {
 }
 
 testSwitchUnknown() {
+  [ "${BASH_VERSINFO[0]}" -lt 4 ] && startSkipping
+
   result=$(git-cc -t 2>&1 | head -1)
   expect='error: unknown switch `-t'
   ${_ASSERT_EQUALS_} "'${expect}'" "'${result%?}'"
 }
 
 testOptionUnknown() {
+  [ "${BASH_VERSINFO[0]}" -lt 4 ] && startSkipping
+
   result=$(git-cc --test 2>&1 | head -1)
   expect='error: unknown option `--test'
   ${_ASSERT_EQUALS_} "'${expect}'" "'${result%?}'"
 }
 
 testMissingSubject() {
+  [ "${BASH_VERSINFO[0]}" -lt 4 ] && startSkipping
+
   result=$(git-cc 2>&1 | head -1)
   expect='fatal: subject must be provided'
   ${_ASSERT_EQUALS_} "'${expect}'" "'${result}'"
 }
 
 testSwitchH() {
+  [ "${BASH_VERSINFO[0]}" -lt 4 ] && startSkipping
+
   result=$(git-cc -h 2>&1 | head -1)
   expect='usage: git cc --subject <subject>'
   ${_ASSERT_EQUALS_} "'${expect}'" "'${result}'"
 }
 
 testOptionHelp() {
+  [ "${BASH_VERSINFO[0]}" -lt 4 ] && startSkipping
+
   result=$(git-cc --help 2>&1 | head -1)
   expect='usage: git cc --subject <subject>'
   ${_ASSERT_EQUALS_} "'${expect}'" "'${result}'"
